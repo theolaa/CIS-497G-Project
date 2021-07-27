446a296c5440d21fbeb2cb0763eb318c6069b717
Theo <theolaa@hotmail.ca>
2021-07-27 19:42:43 +0000

    BMI Calculator Fuctionality Fixed

--- cgi/cgi_test.pl
+++ cgi/cgi_test.pl
@@ -31,7 +31,8 @@ HTML
 print ("<h1> $data{firstname}, Congratulations your account is successfully created! </h1>");
 print ("<h2> Lets calculate your BMI! </h2>");
 print <<HTML;
-<form name="f1" class="modal-content" action="calculateBMI()" method="post">
+
+<div name="f1" class="modal-content">
         <div class="container2">
           <p>Please fill in the details to find your BMI.</p>
           <hr>
@@ -41,58 +42,66 @@ print <<HTML;
         <label for="weight">Weight</label>
         <input type="text" id="weight" name="weight" placeholder="Your weight in kg..">
 
-        <button type="submit" class="signupbtn">Calculate BMI</button>
+        <button type="submit" class="signupbtn" onclick="calculateBMI()">Calculate BMI</button>
 
         <p id="BMI"> </p>
         <p id="BMI-result"> </p>
         </div>
-</form>
+</div>
 HTML
-
-print ("<button type=\"submit\" onclick=\"sendEmail\" class=\"signupbtn\"> Email results to: $data{email} </button>");
 
 print <<HTML;
 <script language="JavaScript">
-var email=$data{email}
+var email="$data{email}"
 function calculateBMI() {
-var weight = document.f1.weight.value;
-var height = document.f1.height.value;
+var weight = document.getElementById("weight").value;
+var height = document.getElementById("height").value;
 var result = "";
 if(weight > 0 && height > 0){	
-var BMI = weight/(height/100*height/100);
-document.getElementByID("BMI").innerHTML= BMI;
+var BMI = Math.round(weight/(height/100*height/100));
+document.getElementById("BMI").innerHTML = BMI;
 if(BMI < 18.5){
-document.getElementByID("BMI-result").innerHTML= "That you are too thin.";
+document.getElementById("BMI-result").innerHTML= "You are too thin.";
 result="That you are too thin.";
 }
 if(BMI > 18.5 && BMI < 25){
-document.getElementByID("BMI-result").innerHTML= "That you are healthy.";
+document.getElementById("BMI-result").innerHTML= "You are healthy.";
 result="That you are healthy.";
 }
 if(BMI > 25){
-document.getElementByID("BMI-result").innerHTML= "That you have overweight.";
-result="That you have overweight.";
+document.getElementById("BMI-result").innerHTML= "You are overweight.";
+result="You are overweight.";
 }
 }
 else{
 alert("Please Fill in everything correctly");
 }
+document.body.innerHTML += "<button type=\\"button\\" onclick=\\"sendEmail()\\" class=\\"signupbtn\\"> Email results to: $data{email} </button>"
 }
 
 function sendEmail() {
-	/*
+	result = document.getElementById("BMI-result").innerText;
+	bmi = document.getElementById("BMI").innerText;
+	///*
 	Email.send({
-	Host: "smtp.gmail.com",
-	Username : "arfhadextra@gmail.com",
-	Password : "arfhad@1234",
-	To : email,
-	From :"arfhadextra@gmail.com",
+	Host: "smtp.elasticemail.com",
+	Username : "account_validation\@learn.taliaq.com",
+	Password : "8CDE0ED2F1E5FA245FBCD6EE5B35CD9D0942",
+	To : "$data{email}",
+	From :"theolaanstra\@gmail.com",
 	Subject : "BMI Results",
-	Body : "Hi \n Your BMI result says" + result ,
+	Body : "Hi \\n Your BMI result says that \\"" + result + "\\"\\r\\nYour BMI is " + bmi
 	}).then(
-		message => alert("mail sent successfully")
+		function(message) {
+		console.log("Email Status: " + message);
+		if (message == "OK") {
+			document.body.innerHTML += "<br>Email sent - check your junk folder!";
+		} else {
+			document.body.innerHTML += "<br>" + message;
+		}
+	}
 	);
-	*/
+	//*/
 	}
 </script>
 </body>
